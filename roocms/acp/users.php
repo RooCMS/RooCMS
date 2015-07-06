e2c3f5b13908d9bba802b0e414498aabec6460ef
Roosso <roosso@ya.ru>
2015-07-07 01:48:23 +0300

    users pm

--- roocms/acp/users.php
+++ roocms/acp/users.php
@@ -6,7 +6,7 @@
  * @author       alex Roosso
  * @copyright    2010-2015 (c) RooCMS
  * @link         http://www.roocms.com
- * @version      1.3
+ * @version      1.3.1
  * @since        $date$
  * @license      http://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -524,8 +524,12 @@ class ACP_USERS {
 
 			$this->count_users($data['gid']);
 
+			# удаляем юзера
 			$db->query("DELETE FROM ".USERS_TABLE." WHERE uid='".$uid."'");
 			$parse->msg("Пользователь #{$uid} был успешно удален из Базы Данных.");
+
+			# удаляем его почту
+			$db->query("DELETE FROM ".USERS_PM_TABLE." WHERE to_uid='".$uid."'");
 		}
 
 		# go
