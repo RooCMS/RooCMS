a163cc30d65154950546080a65579356b3803884
Roosso <roosso@ya.ru>
2015-09-14 04:48:47 +0300

    fix it

--- roocms/class/class_parserDate.php
+++ roocms/class/class_parserDate.php
@@ -1,49 +1,49 @@
 <?php
 /**
-* @package      RooCMS
-* @subpackage	Engine RooCMS classes
-* @author       alex Roosso
-* @copyright    2010-2015 (c) RooCMS
-* @link         http://www.roocms.com
-* @version      2.1
-* @since        $date$
-* @license      http://www.gnu.org/licenses/gpl-3.0.html
-*/
+ * @package      RooCMS
+ * @subpackage   Engine RooCMS classes
+ * @author       alex Roosso
+ * @copyright    2010-2015 (c) RooCMS
+ * @link         http://www.roocms.com
+ * @version      2.1
+ * @since        $date$
+ * @license      http://www.gnu.org/licenses/gpl-3.0.html
+ */
 
 /**
-*   RooCMS - Russian free content managment system
-*   Copyright (C) 2010-2016 alex Roosso aka alexandr Belov info@roocms.com
-*
-*   This program is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see http://www.gnu.org/licenses/
-*
-*
-*   RooCMS - Русская бесплатная система управления сайтом
-*   Copyright (C) 2010-2016 alex Roosso (александр Белов) info@roocms.com
-*
-*   Это программа является свободным программным обеспечением. Вы можете
-*   распространять и/или модифицировать её согласно условиям Стандартной
-*   Общественной Лицензии GNU, опубликованной Фондом Свободного Программного
-*   Обеспечения, версии 3 или, по Вашему желанию, любой более поздней версии.
-*
-*   Эта программа распространяется в надежде, что она будет полезной, но БЕЗ
-*   ВСЯКИХ ГАРАНТИЙ, в том числе подразумеваемых гарантий ТОВАРНОГО СОСТОЯНИЯ ПРИ
-*   ПРОДАЖЕ и ГОДНОСТИ ДЛЯ ОПРЕДЕЛЁННОГО ПРИМЕНЕНИЯ. Смотрите Стандартную
-*   Общественную Лицензию GNU для получения дополнительной информации.
-*
-*   Вы должны были получить копию Стандартной Общественной Лицензии GNU вместе
-*   с программой. В случае её отсутствия, посмотрите http://www.gnu.org/licenses/
-*/
+ *   RooCMS - Russian free content managment system
+ *   Copyright (C) 2010-2016 alex Roosso aka alexandr Belov info@roocms.com
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see http://www.gnu.org/licenses/
+ *
+ *
+ *   RooCMS - Русская бесплатная система управления сайтом
+ *   Copyright (C) 2010-2016 alex Roosso (александр Белов) info@roocms.com
+ *
+ *   Это программа является свободным программным обеспечением. Вы можете
+ *   распространять и/или модифицировать её согласно условиям Стандартной
+ *   Общественной Лицензии GNU, опубликованной Фондом Свободного Программного
+ *   Обеспечения, версии 3 или, по Вашему желанию, любой более поздней версии.
+ *
+ *   Эта программа распространяется в надежде, что она будет полезной, но БЕЗ
+ *   ВСЯКИХ ГАРАНТИЙ, в том числе подразумеваемых гарантий ТОВАРНОГО СОСТОЯНИЯ ПРИ
+ *   ПРОДАЖЕ и ГОДНОСТИ ДЛЯ ОПРЕДЕЛЁННОГО ПРИМЕНЕНИЯ. Смотрите Стандартную
+ *   Общественную Лицензию GNU для получения дополнительной информации.
+ *
+ *   Вы должны были получить копию Стандартной Общественной Лицензии GNU вместе
+ *   с программой. В случае её отсутствия, посмотрите http://www.gnu.org/licenses/
+ */
 
 //#########################################################
 // Anti Hack
@@ -78,8 +78,10 @@ class ParserDate {
 		if($wday == 0) $wday = 7;
 
 		$n = - ($wday - 2);
+		$cal = array();
 
 		for($y=0;$y<=6;$y++) {
+			$row = array();
 			$notEmpty = false;
 
 			for($x=0;$x<7;$x++,$n++) {
@@ -139,7 +141,7 @@ class ParserDate {
 
 		# title month
 		$tm = $this->get_title_month($n_mon);
-			// SEE---> $tm = $this->get_title_month($n_mon, true, $short);
+		// SEE---> $tm = $this->get_title_month($n_mon, true, $short);
 
 		# форматируем дату
 		$date = $tday.$n_day." ".$tm." ".$n_year."г. ".$time;
@@ -215,6 +217,8 @@ class ParserDate {
 	 *               tmonth - название месяца
 	 */
 	public function unix_to_rus_array($udate) {
+
+		$ar = array();
 
 		$ar['day']	= date("d", $udate);
 		$ar['month']	= date("m", $udate);
@@ -336,6 +340,7 @@ class ParserDate {
 		$nw = round($nw);
 
 		# full day			# short day
+		$day	= array();		$sday	 = array();
 		$day[0] = 'Воскресенье';	$sday[0] = 'Вс';
 		$day[1] = 'Понедельник';	$sday[1] = 'Пн';
 		$day[2] = 'Вторник';		$sday[2] = 'Вт';
@@ -365,12 +370,14 @@ class ParserDate {
 
 		# month
 		if(!$short) {
+			$month = array();
 			$month[1]	= 'Январ';	$month[2]	= 'Феврал';	$month[3]	= 'Март';
 			$month[4]	= 'Апрел';	$month[5]	= 'Ма';		$month[6]	= 'Июн';
 			$month[7]	= 'Июл';	$month[8]	= 'Август';	$month[9]	= 'Сентябр';
 			$month[10]	= 'Октябр';	$month[11]	= 'Ноябр';	$month[12]	= 'Декабр';
 		}
 		else {
+			$month = array();
 			$month[1]	= 'Янв';	$month[2]	= 'Фев';	$month[3]	= 'Мар';
 			$month[4]	= 'Апр';	$month[5]	= 'Ма';		$month[6]	= 'Июн';
 			$month[7]	= 'Июл';	$month[8]	= 'Авг';	$month[9]	= 'Сен';
