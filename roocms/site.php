0a1059341a6d6ab58000f87557753b866f3b2753
alexandr Belov <roosso@ya.ru>
2018-08-26 11:34:21 +0300

    update copyright

--- roocms/site.php
+++ roocms/site.php
@@ -4,36 +4,8 @@
  *   Copyright © 2010-2018 alexandr Belov aka alex Roosso. All rights reserved.
  *   Contacts: <info@roocms.com>
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
+ *   You should have received a copy of the GNU General Public License v3
  *   along with this program.  If not, see http://www.gnu.org/licenses/
- *
- *
- *   RooCMS - Бесплатная система управления контентом с открытым исходным кодом
- *   Copyright © 2010-2018 александр Белов (alex Roosso). Все права защищены.
- *   Для связи: <info@roocms.com>
- *
- *   Это программа является свободным программным обеспечением. Вы можете
- *   распространять и/или модифицировать её согласно условиям Стандартной
- *   Общественной Лицензии GNU, опубликованной Фондом Свободного Программного
- *   Обеспечения, версии 3 или, по Вашему желанию, любой более поздней версии.
- *
- *   Эта программа распространяется в надежде, что она будет полезной, но БЕЗ
- *   ВСЯКИХ ГАРАНТИЙ, в том числе подразумеваемых гарантий ТОВАРНОГО СОСТОЯНИЯ ПРИ
- *   ПРОДАЖЕ и ГОДНОСТИ ДЛЯ ОПРЕДЕЛЁННОГО ПРИМЕНЕНИЯ. Смотрите Стандартную
- *   Общественную Лицензию GNU для получения дополнительной информации.
- *
- *   Вы должны были получить копию Стандартной Общественной Лицензии GNU вместе
- *   с программой. В случае её отсутствия, посмотрите http://www.gnu.org/licenses/
  */
 
 /**
