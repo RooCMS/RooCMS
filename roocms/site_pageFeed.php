683b97c20f42f4b5f0fcf63c25b50940d51ccda7
alexandr Belov <roosso@ya.ru>
2019-01-18 18:30:05 +0300

    fix

--- roocms/site_pageFeed.php
+++ roocms/site_pageFeed.php
@@ -290,7 +290,6 @@ class PageFeed {
 			$data[$k]['datepub'] = $parse->date->unix_to_rus($data[$k]['date_publications']);
 			$data[$k]['image'] = $img->load_images("feeditemid=".$data[$k]['id']."", 0, 1);
 		}
-		//debug($data);
 
 		# return
 		return $data;
