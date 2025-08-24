8ee5761f0daa1e793e127acfde656ccca076dc28
alex Roosso <roosso@ya.ru>
2025-08-24 09:17:34 +0300

    up
    update copyright

--- acp.php
+++ acp.php
@@ -7,7 +7,7 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.html
  *
  * You should have received a copy of the GNU General Public License v3
- * along with this program.  If not, see http://www.gnu.org/licenses/
+ * along with this program. If not, see http://www.gnu.org/licenses/
  */
 
 
