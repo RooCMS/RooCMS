22331cc648b43dd5a4aef1511984d40c1236afde
alex Roosso <roosso@ya.ru>
2025-10-26 23:24:18 +0300

    cosmetic .13

--- themes/default/system/acp/users.php
+++ themes/default/system/acp/users.php
@@ -140,13 +140,13 @@ ob_start();
                                                 <div class="ml-4">
                                                     <div class="text-sm font-medium text-zinc-900">
                                                         <span x-text="user.login"></span>
-                                                        <span x-show="user.is_verified" class="ml-1 inline-flex items-center px-1.5 py-0.5 rounded text-xs font-medium bg-green-100 text-green-800">
+                                                        <span x-show="user.is_verified" class="ml-1 inline-flex items-center px-1.5 py-0.5 rounded-sm text-xs font-medium bg-green-100 text-green-800">
                                                             <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
                                                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
                                                             </svg>
                                                             Verified
                                                         </span>
-                                                        <span x-show="isCurrentUser(user.id)" class="ml-1 inline-flex items-center px-1.5 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800">
+                                                        <span x-show="isCurrentUser(user.id)" class="ml-1 inline-flex items-center px-1.5 py-0.5 rounded-sm text-xs font-medium bg-purple-100 border border-purple-200 text-purple-700">
                                                             <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
                                                                 <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
                                                             </svg>
