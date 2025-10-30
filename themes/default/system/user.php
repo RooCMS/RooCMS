5881ba3edf4f3d984aff1c71392efc8bb49a44de
alex Roosso <roosso@ya.ru>
2025-10-31 01:35:24 +0300

    cosmetic .18

--- themes/default/system/user.php
+++ themes/default/system/user.php
@@ -77,9 +77,6 @@ if (userId) {
                 </svg>
             </div>
             <div x-show="!showLoginLink">
-                <svg class="w-8 h-8 text-red-600 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16.5c-.77.833.192 2.5 1.732 2.5z"></path>
-                </svg>
                 <h3 class="text-lg font-semibold text-red-900 mb-2">Error Loading Profile</h3>
             </div>
             <div x-show="showLoginLink">
